2ecb0aff7d9f5fe7a7fb4e66b57b61dea85ef3b5
Ali Rahbar <alirahbar2005@gmail.com>
2025-01-17 16:39:29 -0500

    Added heap size function

--- heap.c
+++ heap.c
@@ -2,6 +2,45 @@
 // Created by Ali Rahbar on 2025-01-17.
 //
 
-int main() {
+#include <printf.h>
+
+// Structure for the heap
+struct heap
+{
+    int *arr;
+    int size;
+    int capacity;
+};
+
+struct heapNode
+{
+    int value;
+    int priority;
+};
+
+// Prototypes for the functions in heap.c
+void enqueue(struct heap curr_heap, int value, int priority);
+int dequeue(struct heap curr_heap);
+int peek(struct heap curr_heap);
+int is_empty(struct heap curr_heap);
+int size(struct heap curr_heap);
+
+// Main function
+int main()
+{
+    printf("Hello, World!\n");
     return 0;
+}
+
+// Implementation of the heap functions
+
+
+/**
+ * Function to get the size of the heap
+ * @param curr_heap the heap
+ * @return size of the heap
+ */
+int size(struct heap curr_heap)
+{
+    return curr_heap.size;
 }
